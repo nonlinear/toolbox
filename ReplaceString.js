0ad5bfc467f5f24481c46f4aa8aaed3b62ca4cab
nonlinear <info@nicholasfrota.com>
2020-12-17 10:02:39 -0500

    replaceString

--- ReplaceString.js
+++ ReplaceString.js
@@ -1,10 +1,12 @@
-	function ReplaceString(initialString, finalString) {
-		var watch = selectAll(".watch");
-		for (var i = 0; i < watch.length; i++) {
-			var target = watch[i].html();
-			var targetSplit = target.split(initialString);
-			var targetResult = targetSplit[0] + finalString + targetSplit[1]; 
-			watch[i].html(targetResult, false);
-		}
-
-	}
+	function replaceString(initialString, finalString, target) {
+    if(!target) {
+      var watch = selectAll(".replaceString");
+    } else {
+      var watch = selectAll(target);
+    }
+    for (var i = 0; i < watch.length; i++) {
+     var target = watch[i].html();
+     var targetResult = target.replaceAll(initialString, finalString);
+     watch[i].html(targetResult, false);
+   }
+ }
